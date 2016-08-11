217a667329721ae33428906c5adf38cc3d80a110
Max Carlquist <tenkaklet@gmail.com>
2016-08-12 01:27:51 +0200

    achieved appending with jQuery

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 $(function () {
 
   var NewsUrl = 'https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=e52ac532f5764bebab21059964519cbc';
-
+  var keyDiv = $('.key');
+  var valueDiv = $('.value');
   $.getJSON(NewsUrl, function (data) {
-    console.log(data);
+    var base = data.articles[0];
+    console.log(base);
+    $.each(base, function (i, v) {
+      var keyTemp = '<li>' + i + '</li>';
+      keyDiv.append(keyTemp);
+      var valTemp = '<li>' + v + '</li>';
+      valueDiv.append(valTemp);
+    });
   });
 });
